ba7798784d04dddf14dc18e4e1fcd92c434f0977
Sergiu Deitsch <sergiu.deitsch@gmail.com>
2024-11-02 14:16:03 +0100

    l3build support

---
+++ build.lua
@@ -0,0 +1,23 @@
+module = 'rebuttal'
+
+sourcefiledir = '.'
+sourcefiles = {'rebuttal.sty'}
+
+demofiles = {'examples/*.tex'}
+
+uploadconfig = {
+    pkg = 'rebuttal',
+    summary = 'Structured rebuttals in LaTeX',
+    description = 'The package provides means for writing structured journal and conference paper rebuttals',
+    author = 'Sergiu Deitsch',
+    email = 'sergiu.deitsch@gmail.com',
+    ctanPath = 'macros/latex/contrib',
+    repository = 'https://github.com/sergiud/rebuttal',
+    bugtracker = 'https://github.com/sergiud/rebuttal/issues',
+    update = false,
+    uploader = 'Sergiu Deitsch',
+    version = '0.1.0',
+    topic = {'editorial', 'notes'},
+    license = 'lppl1.3c',
+    announcement = 'The rebuttal package provides means for writing structured journal and conference paper rebuttals.',
+}
