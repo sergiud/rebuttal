9ac729d7b8bb1d683407baa5c249e87327547714
Sergiu Deitsch <sergiu.deitsch@gmail.com>
2024-11-02 21:14:48 +0100

    l3build: use latexmk to compile examples

--- build.lua
+++ build.lua
@@ -3,6 +3,29 @@ module = 'rebuttal'
 sourcefiledir = '.'
 
 demofiles = {'??-example.tex'}
+typesetfiles = {'rebuttal.dtx', '??-example.tex'}
+
+function typetset_example(fn)
+    typesetexe = ''
+    local texfn = unpackdir .. '/' .. fn
+
+    result = runcmd(string.format("latexmk -pdf -cd '%s'", texfn))
+    if not result == 0 then return result end
+
+    local pdffn = string.gsub(fn, '.tex$', '.pdf', 1)
+    local destdir = ctandir .. '/' .. module .. '/examples'
+
+    result = mkdir(destdir)
+    if not result == 0 then return result end
+
+    result = cp(fn, unpackdir, destdir)
+    if not result == 0 then return result end
+
+    return cp(pdffn, unpackdir, destdir)
+end
+
+specialtypesetting = {}
+specialtypesetting['01-example.tex'] = {func = typetset_example}
 
 uploadconfig = {
     pkg = 'rebuttal',
@@ -13,10 +36,10 @@ uploadconfig = {
     ctanPath = 'macros/latex/contrib',
     repository = 'https://github.com/sergiud/rebuttal',
     bugtracker = 'https://github.com/sergiud/rebuttal/issues',
-    update = false,
+    update = true,
     uploader = 'Sergiu Deitsch',
     version = '0.1.0',
     topic = {'editorial', 'notes'},
     license = 'lppl1.3c',
-    announcement = 'The rebuttal package provides means for writing structured journal and conference paper rebuttals.',
+    announcement = '',
 }
